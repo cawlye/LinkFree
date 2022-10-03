fe6cf3a80baed8a8de7a221cf2ee6118edccdb49
Vignesh Shetty <vigneshshetty.in@gmail.com>
2022-10-03 04:41:29 +0000

    Migrated Linkfree from Delta to GDSC

--- data/data.js
+++ data/data.js
@@ -1,32 +1,39 @@
 export const LinkFree = {
   links: [
     {
+      id: 0,
+      name: "GDSC SJEC",
+      url: "https://gdscsjec.in/",
+      img: "https://img.icons8.com/color/344/internet--v1.png",
+    },
+    {
       id: 1,
       name: "Discord",
       url: "https://discord.gg/fjQWQZ",
-      img: "https://img.icons8.com/ios/250/000000/discord-logo--v1.png",
+      img: "https://img.icons8.com/fluency/344/discord-logo.png",
     },
     {
       id: 2,
       name: "Github",
-      url: "https://github.com/Delta-SJEC",
-      img: "https://img.icons8.com/ios/50/000000/github--v1.png",
+      url: "https://github.com/GDSC-SJEC",
+      img: "https://img.icons8.com/ios/344/github--v1.png",
     },
     {
       id: 3,
       name: "Instagram",
-      url: "https://www.instagram.com/deltasjec/",
-      img: "https://img.icons8.com/ios/250/000000/instagram-new--v1.png",
+      url: "https://www.instagram.com/gdsc.sjec/",
+      img: "https://img.icons8.com/fluency/344/instagram-new.png",
     },
     {
       id: 4,
       name: "Linkedin",
-      url: "https://www.instagram.com/deltasjec/",
-      img: "https://img.icons8.com/ios/250/000000/linkedin.png",
+      url: "https://www.linkedin.com/company/gdsc-sjec/",
+      img: "https://img.icons8.com/color/344/linkedin-circled--v1.png",
     },
   ],
   header: {
-    text: "Delta SJEC",
+    text: "GDSC SJEC",
     subText: "Building an Inclusive Developer Group!",
+    image: "https://avatars.githubusercontent.com/u/93979833?s=200&v=4",
   },
 };
